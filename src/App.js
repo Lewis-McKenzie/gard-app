7b1e44528cb64641143406d6ea2e8ed31ab7319a
Lewis-McKenzie <53063230+Lewis-McKenzie@users.noreply.github.com>
2023-09-02 15:08:19 +0100

    initial homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import 'react'
+import api from './api/axiosConfig'
+import Layout from './components/Layout';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/home/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Routes>
+          <Route path="/" element={<Layout/>}>
+            <Route path="/" element={<Home/>}></Route>
+          </Route>
+      </Routes>
     </div>
   );
 }
